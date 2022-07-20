b851db2fceeffd85f42db44ce86d412906ea0a59
vikas yadav <vikas.yadav@myparkplus.com>
2022-07-20 11:32:23 +0530

    initial commit

--- test_api.py
+++ test_api.py
@@ -1,24 +1,34 @@
 import requests
-# import slackclient
-# import slack
-# SLACK_TOKEN = 'xoxb-151880674451-3762166660774-oIGiy4702S5w6O0SRSAiY8yV'
-#
-# client = slack.WebClient(token=SLACK_TOKEN)
-# # client.chat_postMessage(channel='#qa-alerts', text='Hello')
+import slackclient
 
-def test_Hardware():
+SLACK_TOKEN = 'xoxb-151880674451-3762166660774-oIGiy4702S5w6O0SRSAiY8yV'
+# import os
+# import slack
+# from slack.errors import SlackApiError
+#
+# slack_token = os.environ["xoxb-151880674451-3762166660774-oIGiy4702S5w6O0SRSAiY8yV"]
+# client = slack.WebClient(token=slack_token)
+# client.chat_postMessage(channel='#qa-alerts', text='Hello')
+# import pytest
+
+def test_hardware():
     url = 'https://hardware.parkplus.io/api/v2/iot_v2/device/get-unacknowledged-data/'
 
     headers = {'Content-Type': 'application/json'}
 
     data = {
-    "device_id": 16485,
-    "project_id": 13685
+    "device_id": 17449,
+    "project_id": 12996
             }
 
     response = requests.post(url, headers=headers, json=data)
     print(response.status_code)
-    print(response.json())
+    print(len(response.content))
+    if len(response.content) > 110:
+        print('Unacknowledged data is present')
+    else:
+        print('No unacknowledged data')
+
     #assert response.json()['message'] == 'Successful'
     try:
         assert response.status_code == 200
